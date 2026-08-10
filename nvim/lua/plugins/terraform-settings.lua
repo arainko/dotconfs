6b463a3514fd21cac61de61b95ff0978668157ec
RAINA029 <aleksander.rainko.-ND@disney.com>
2026-08-10 16:04:11 +0200

    add add-worktree

---
+++ nvim/lua/plugins/terraform-settings.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client and client.name == "terraformls" then
+            client.server_capabilities.semanticTokensProvider = nil
+          end
+        end,
+      })
+    end,
+  },
+}
